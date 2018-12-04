8cac4dd64cc7641bb0e4703131934b6ccc52cc19
qingwuguo <qingwuguo@supernano.com>
2018-12-04 17:59:38 +0800

    *

--- context/context.go
+++ context/context.go
@@ -1,6 +1,8 @@
 package context
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // Context struct
 type Context struct {
@@ -10,4 +12,19 @@ type Context struct {
 
 	Writer  http.ResponseWriter
 	Request *http.Request
+}
+
+// Query returns the keyed url query value if it exists
+func (ctx *Context) Query(key string) string {
+	value, _ := ctx.GetQuery(key)
+	return value
+}
+
+// GetQuery is like Query(), it returns the keyed url query value
+func (ctx *Context) GetQuery(key string) (string, bool) {
+	req := ctx.Request
+	if values, ok := req.URL.Query()[key]; ok && len(values) > 0 {
+		return values[0], true
+	}
+	return "", false
 }
--- server/server.go
+++ server/server.go
@@ -1,15 +1,11 @@
 package server
 
 import (
-	"encoding/xml"
+	"encoding/json"
 	"fmt"
 	"github.com/yuyan2077/payjs/context"
-	"github.com/yuyan2077/payjs/docs/wechat-develop/message"
-	"github.com/yuyan2077/payjs/docs/wechat-develop/util"
-	"io/ioutil"
-	"reflect"
-	"runtime/debug"
-	"strconv"
+	"github.com/yuyan2077/payjs/util"
+	"net/url"
 )
 
 //Server struct
@@ -18,10 +14,10 @@ type Server struct {
 
 	debug bool
 
-	requestRawXMLMsg  []byte
-	requestMsg        message.MixMessage
-	responseRawXMLMsg []byte
-	responseMsg       interface{}
+	messageHandler func(message Message)
+
+	requestMsg  Message
+	responseMsg interface{}
 }
 
 // Message PayJS支付成功异步通知过来的内容
@@ -33,7 +29,7 @@ type Message struct {
 	TransactionID string `json:"transaction_id"` // 必填	微信用户手机显示订单号
 	TimeEnd       string `json:"time_end"`       // 必填	支付成功时间
 	Openid        string `json:"openid"`         // 必填	用户OPENID标示，本参数没有实际意义，旨在方便用户端区分不同用户
-	Attach        string `json:"attach"`         // 非必填	用户自定义数据
+	Attach        string `json:"attach"`         // 非必填 用户自定义数据
 	MchID         string `json:"mchid"`          // 必填	PAYJS 商户号
 	Sign          string `json:"sign"`           // 必填	数据签名 详见签名算法
 }
@@ -50,196 +46,56 @@ func (srv *Server) SetDebug(debug bool) {
 	srv.debug = debug
 }
 
-//Serve 处理微信的请求消息
+//SetMessageHandler 设置用户自定义处理PayJS支付成功推送消息的方法
+func (srv *Server) SetMessageHandler(handler func(message Message)) {
+	srv.messageHandler = handler
+}
+
+//Serve 处理PayJS支付成功推送的消息
 func (srv *Server) Serve() error {
-	if !srv.Validate() {
-		return fmt.Errorf("请求校验失败")
-	}
-
-	echostr, exists := srv.GetQuery("echostr")
-	if exists {
-		srv.String(echostr)
-		return nil
-	}
-
-	response, err := srv.handleRequest()
+	err := srv.handleRequest()
 	if err != nil {
 		return err
 	}
 
-	//debug
-	//fmt.Println("request msg = ", string(srv.requestRawXMLMsg))
-
-	return srv.buildResponse(response)
+	return srv.SendResponseMsg()
 }
 
-//Validate 校验请求是否合法
-func (srv *Server) Validate() bool {
-	if srv.debug {
-		return true
-	}
-	timestamp := srv.Query("timestamp")
-	nonce := srv.Query("nonce")
-	signature := srv.Query("signature")
-	return signature == util.Signature(srv.Token, timestamp, nonce)
-}
-
-//HandleRequest 处理微信的请求
-func (srv *Server) handleRequest() (reply *message.Reply, err error) {
-	//set isSafeMode
-	srv.isSafeMode = false
-	encryptType := srv.Query("encrypt_type")
-	if encryptType == "aes" {
-		srv.isSafeMode = true
-	}
-
-	//set openID
-	srv.openID = srv.Query("openid")
-
-	var msg interface{}
-	msg, err = srv.getMessage()
+//HandleRequest 处理微信的请求。消息有可能反复推送，所以要去重；可能会增加数据，所以要取最新的；检测金额是否与自己订单相同；
+func (srv *Server) handleRequest() (err error) {
+	message, err := srv.getMessage()
 	if err != nil {
 		return
 	}
-	mixMessage, success := msg.(message.MixMessage)
-	if !success {
-		err = errors.New("消息类型转换失败")
-	}
-	srv.requestMsg = mixMessage
-	reply = srv.messageHandler(mixMessage)
+	srv.messageHandler(message)
 	return
 }
 
-//GetOpenID return openID
-func (srv *Server) GetOpenID() string {
-	return srv.openID
+//getMessage 解析PayJS支付成功推送的消息
+func (srv *Server) getMessage() (message Message, err error) {
+	if err = json.NewDecoder(srv.Request.Body).Decode(&message); err != nil {
+		return message, fmt.Errorf("从body中解析json失败,err=%v", err)
+	}
+
+	//验证消息签名
+	msgSignature := srv.Query("sign")
+	var p = url.Values{}
+	jsonbs, _ := json.Marshal(message)
+	jsonmap := make(map[string]interface{})
+	json.Unmarshal(jsonbs, &jsonmap)
+	for k, v := range jsonmap {
+		p.Add(k, fmt.Sprintf("%v", v))
+	}
+	msgSignatureGen := util.Signature(p, srv.Context.Key)
+	if msgSignature != msgSignatureGen {
+		return message, fmt.Errorf("消息不合法，验证签名失败")
+	}
+
+	return message, err
 }
 
-//getMessage 解析微信返回的消息
-func (srv *Server) getMessage() (interface{}, error) {
-	var rawXMLMsgBytes []byte
-	var err error
-	if srv.isSafeMode {
-		var encryptedXMLMsg message.EncryptedXMLMsg
-		if err := xml.NewDecoder(srv.Request.Body).Decode(&encryptedXMLMsg); err != nil {
-			return nil, fmt.Errorf("从body中解析xml失败,err=%v", err)
-		}
-
-		//验证消息签名
-		timestamp := srv.Query("timestamp")
-		srv.timestamp, err = strconv.ParseInt(timestamp, 10, 32)
-		if err != nil {
-			return nil, err
-		}
-		nonce := srv.Query("nonce")
-		srv.nonce = nonce
-		msgSignature := srv.Query("msg_signature")
-		msgSignatureGen := util.Signature(srv.Token, timestamp, nonce, encryptedXMLMsg.EncryptedMsg)
-		if msgSignature != msgSignatureGen {
-			return nil, fmt.Errorf("消息不合法，验证签名失败")
-		}
-
-		//解密
-		srv.random, rawXMLMsgBytes, err = util.DecryptMsg(srv.AppID, encryptedXMLMsg.EncryptedMsg, srv.EncodingAESKey)
-		if err != nil {
-			return nil, fmt.Errorf("消息解密失败, err=%v", err)
-		}
-	} else {
-		rawXMLMsgBytes, err = ioutil.ReadAll(srv.Request.Body)
-		if err != nil {
-			return nil, fmt.Errorf("从body中解析xml失败, err=%v", err)
-		}
-	}
-
-	srv.requestRawXMLMsg = rawXMLMsgBytes
-
-	return srv.parseRequestMessage(rawXMLMsgBytes)
-}
-
-func (srv *Server) parseRequestMessage(rawXMLMsgBytes []byte) (msg message.MixMessage, err error) {
-	msg = message.MixMessage{}
-	err = xml.Unmarshal(rawXMLMsgBytes, &msg)
-	return
-}
-
-//SetMessageHandler 设置用户自定义的回调方法
-func (srv *Server) SetMessageHandler(handler func(message.MixMessage) *message.Reply) {
-	srv.messageHandler = handler
-}
-
-func (srv *Server) buildResponse(reply *message.Reply) (err error) {
-	defer func() {
-		if e := recover(); e != nil {
-			err = fmt.Errorf("panic error: %v\n%s", e, debug.Stack())
-		}
-	}()
-	if reply == nil {
-		//do nothing
-		return nil
-	}
-	msgType := reply.MsgType
-	switch msgType {
-	case message.MsgTypeText:
-	case message.MsgTypeImage:
-	case message.MsgTypeVoice:
-	case message.MsgTypeVideo:
-	case message.MsgTypeMusic:
-	case message.MsgTypeNews:
-	case message.MsgTypeTransfer:
-	default:
-		err = message.ErrUnsupportReply
-		return
-	}
-
-	msgData := reply.MsgData
-	value := reflect.ValueOf(msgData)
-	//msgData must be a ptr
-	kind := value.Kind().String()
-	if "ptr" != kind {
-		return message.ErrUnsupportReply
-	}
-
-	params := make([]reflect.Value, 1)
-	params[0] = reflect.ValueOf(srv.requestMsg.FromUserName)
-	value.MethodByName("SetToUserName").Call(params)
-
-	params[0] = reflect.ValueOf(srv.requestMsg.ToUserName)
-	value.MethodByName("SetFromUserName").Call(params)
-
-	params[0] = reflect.ValueOf(msgType)
-	value.MethodByName("SetMsgType").Call(params)
-
-	params[0] = reflect.ValueOf(util.GetCurrTs())
-	value.MethodByName("SetCreateTime").Call(params)
-
-	srv.responseMsg = msgData
-	srv.responseRawXMLMsg, err = xml.Marshal(msgData)
-	return
-}
-
-//Send 将自定义的消息发送
-func (srv *Server) Send() (err error) {
-	replyMsg := srv.responseMsg
-	if srv.isSafeMode {
-		//安全模式下对消息进行加密
-		var encryptedMsg []byte
-		encryptedMsg, err = util.EncryptMsg(srv.random, srv.responseRawXMLMsg, srv.AppID, srv.EncodingAESKey)
-		if err != nil {
-			return
-		}
-		//TODO 如果获取不到timestamp nonce 则自己生成
-		timestamp := srv.timestamp
-		timestampStr := strconv.FormatInt(timestamp, 10)
-		msgSignature := util.Signature(srv.Token, timestampStr, srv.nonce, string(encryptedMsg))
-		replyMsg = message.ResponseEncryptedXMLMsg{
-			EncryptedMsg: string(encryptedMsg),
-			MsgSignature: msgSignature,
-			Timestamp:    timestamp,
-			Nonce:        srv.nonce,
-		}
-	}
-	if replyMsg != nil {
-		srv.XML(replyMsg)
-	}
+func (srv *Server) SendResponseMsg() (err error) {
+	srv.Writer.Header().Set("Content-Type", "application/json")
+	srv.Writer.Write([]byte("ok"))
 	return
 }
