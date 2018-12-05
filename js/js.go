8abb9b0cfd9a359f59d880ed1cdb4f71022972f8
qingwuguo <qingwuguo@icloud.com>
2018-12-05 22:34:23 +0800

    *

---
+++ js/js.go
@@ -0,0 +1,85 @@
+package js
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/yuyan2077/payjs/context"
+	"github.com/yuyan2077/payjs/util"
+)
+
+const getJsApiURL = "https://payjs.cn/api/jsapi"
+const getOpenidURL = "https://payjs.cn/api/openid"
+
+// Js struct
+type Js struct {
+	*context.Context
+
+	debug bool
+
+	openID string
+}
+
+// JsApiRequest
+type JsApiRequest struct {
+	MchID      string `json:"mchid"`        //Y	商户号
+	TotalFee   int    `json:"total_fee"`    //Y	金额。单位：分
+	OutTradeNo string `json:"out_trade_no"` //Y	用户端自主生成的订单号，在用户端要保证唯一性
+	Body       string `json:"body"`         //N	订单标题
+	Attach     string `json:"attach"`       //N	用户自定义数据，在notify的时候会原样返回
+	NotifyUrl  string `json:"notify_url"`   //N	接收微信支付异步通知的回调地址。必须为可直接访问的URL，不能带参数、session验证、csrf验证。留空则不通知
+	Openid     string `json:"openid"`       //Y	用户openid
+	Sign       string `json:"sign"`         //Y	数据签名 详见签名算法
+}
+
+// JsApiResponse
+type JsApiResponse struct {
+	ReturnCode   int    `json:"return_code"`    //Y	0:失败 1:成功
+	ReturnMsg    string `json:"return_msg"`     //Y	失败原因
+	PayJSOrderID string `json:"payjs_order_id"` //Y	PAYJS 侧订单号
+	JsApi        string `json:"jsapi"`          //N	用于发起支付的支付参数
+	Sign         string `json:"sign"`           //Y	数据签名
+}
+
+//NewJs init
+func NewJs(context *context.Context) *Js {
+	js := new(Js)
+	js.Context = context
+	return js
+}
+
+// GetRequestOpenidUrl 获取浏览器跳转的url
+func (js *Js) GetRequestOpenidUrl(callbackUrl string) string {
+	return getOpenidURL + "?callback_url=" + callbackUrl
+}
+
+//Serve 处理PayJS支付成功推送的消息
+func (js *Js) Serve() (err error) {
+	js.openID = js.Query("openid")
+	return
+}
+
+// GetJsApi 获取发起支付所需要的参数
+func (js *Js) GetJsApi(jsApiRequest *JsApiRequest) (jsApiResponse JsApiResponse, err error) {
+	sign := util.Signature(jsApiRequest, js.Context.Key)
+	jsApiRequest.Sign = sign
+	response, err := util.PostJSON(getJsApiURL, jsApiRequest)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(response, &jsApiResponse)
+	if err != nil {
+		return
+	}
+	if jsApiResponse.ReturnCode == 0 {
+		err = fmt.Errorf("GetJsApi Error , errcode=%d , errmsg=%s", jsApiResponse.ReturnCode, jsApiResponse.ReturnMsg)
+		return
+	}
+	// 检测sign
+	msgSignature := jsApiResponse.Sign
+	msgSignatureGen := util.Signature(jsApiResponse, js.Context.Key)
+	if msgSignature != msgSignatureGen {
+		err = fmt.Errorf("消息不合法，验证签名失败")
+		return
+	}
+	return
+}
--- miniapp/miniapp.go
+++ miniapp/miniapp.go
@@ -1,6 +1,9 @@
 package miniapp
 
-import "github.com/yuyan2077/payjs/context"
+import (
+	"github.com/yuyan2077/payjs/context"
+	"github.com/yuyan2077/payjs/util"
+)
 
 // MiniApp struct
 type MiniApp struct {
@@ -9,14 +12,14 @@ type MiniApp struct {
 
 // OrderInfo 后端按照下面参数构造订单参数，返回给前端
 type OrderInfo struct {
-	MchID      string `json:"mch_id"`       // 必填 商户号
-	TotalFee   int    `json:"total_fee"`    // 必填 金额。单位：分
-	OutTradeNo string `json:"out_trade_no"` // 必填 用户端自主生成的订单号
-	Body       string `json:"body"`         // 非必填 订单标题
-	Attach     string `json:"attach"`       // 非必填 用户自定义数据，在notify的时候会原样返回
-	NotifyUrl  string `json:"notify_url"`   // 非必填 异步通知地址
-	Nonce      string `json:"nonce"`        // 必填 随机字符串
-	Sign       string `json:"sign"`         // 必填 数据签名 详见签名算法
+	MchID      string `json:"mch_id"`       //Y 商户号
+	TotalFee   int    `json:"total_fee"`    //Y 金额。单位：分
+	OutTradeNo string `json:"out_trade_no"` //Y 用户端自主生成的订单号
+	Body       string `json:"body"`         //N 订单标题
+	Attach     string `json:"attach"`       //N 用户自定义数据，在notify的时候会原样返回
+	NotifyUrl  string `json:"notify_url"`   //N 异步通知地址
+	Nonce      string `json:"nonce"`        //Y 随机字符串
+	Sign       string `json:"sign"`         //Y 数据签名 详见签名算法
 }
 
 //NewMiniApp init
@@ -24,4 +27,11 @@ func NewMiniApp(context *context.Context) *MiniApp {
 	miniApp := new(MiniApp)
 	miniApp.Context = context
 	return miniApp
+}
+
+// GetOrderInfo 获取小程序跳转所需的参数
+func (miniApp *MiniApp) GetOrderInfo(orderInfo *OrderInfo) *OrderInfo {
+	sign := util.Signature(orderInfo, miniApp.Context.Key)
+	orderInfo.Sign = sign
+	return orderInfo
 }
