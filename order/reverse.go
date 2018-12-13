2602c8455fca3c3760bb0eb6739cf80ed6018280
qingwuguo <qingwuguo@supernano.com>
2018-12-13 15:43:52 +0800

    增加撤销订单接口

---
+++ order/reverse.go
@@ -0,0 +1,49 @@
+package order
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/yuyan2077/payjs/util"
+)
+
+const getReverseURL = "https://payjs.cn/api/reverse"
+
+// ReverseRequest 请求参数
+type ReverseRequest struct {
+	PayJSOrderID string `json:"payjs_order_id"` //Y	PAYJS 平台订单号
+	Sign         string `json:"sign"`           //Y	数据签名 详见签名算法
+}
+
+// ReverseResponse PayJS返回参数
+type ReverseResponse struct {
+	ReturnCode   int    `json:"return_code"`    //Y	1:请求成功 0:请求失败
+	ReturnMsg    string `json:"return_msg"`     //Y	返回消息
+	PayJSOrderID string `json:"payjs_order_id"` //Y	PAYJS 平台订单号
+	Sign         string `json:"sign"`           //Y	数据签名 详见签名算法
+}
+
+// Reverse 撤销订单
+func (order *Order) Reverse(reverseRequest *ReverseRequest) (reverseResponse ReverseResponse, err error) {
+	sign := util.Signature(reverseRequest, order.Context.Key)
+	reverseRequest.Sign = sign
+	response, err := util.PostJSON(getReverseURL, reverseRequest)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(response, &reverseResponse)
+	if err != nil {
+		return
+	}
+	if reverseResponse.ReturnCode == 0 {
+		err = fmt.Errorf("Reverse  Error , errcode=%d , errmsg=%s", reverseResponse.ReturnCode, reverseResponse.ReturnMsg)
+		return
+	}
+	// 检测sign
+	msgSignature := reverseResponse.Sign
+	msgSignatureGen := util.Signature(reverseResponse, order.Context.Key)
+	if msgSignature != msgSignatureGen {
+		err = fmt.Errorf("消息不合法，验证签名失败")
+		return
+	}
+	return
+}
