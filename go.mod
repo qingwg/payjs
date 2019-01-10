2b261cb3f4b538e4c3f66ce4c5ac9207059a9057
qingwuguo <qingwuguo@icloud.com>
2019-01-10 22:30:19 +0800

    *

--- go.mod
+++
@@ -1,3 +0,0 @@
-module payjs
-
-require github.com/qingwg/payjs v0.0.0-20190110111438-2f1581ee6678
--- vendor/github.com/qingwg/payjs/context/context.go
+++
@@ -1,8 +0,0 @@
-package context
-
-// Context struct
-type Context struct {
-	Key       string
-	MchID     string
-	NotifyUrl string
-}
